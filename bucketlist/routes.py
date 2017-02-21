dcca30712ccfaff5ae9dba00d31b5b83b865b91e
andela-ik <ian.kingori@andela.com>
2017-02-21 15:13:16 +0300

    [Chore] Refactored Routes

--- bucketlist/routes.py
+++ bucketlist/routes.py
@@ -1,17 +1,15 @@
 from flask_restful import Api
-from bucketlist.views import Auth, BucketList, BucketListItem, Index
+from bucketlist.views import Auth, BucketLists, BucketListItem
 
 api = Api()
 
-#default url
-api.add_resource(Index, '/')
-
-#route to handle user registration and login
+# route to handle user registration and login
 api.add_resource(Auth, '/auth/<string:action>')
 
-#create and list bucket lists
-#get, update and delete single bucket list
-api.add_resource(BucketList, '/bucketlists/<int:id>')
+# create and list bucket lists
+# get, update and delete single bucket list
+api.add_resource(BucketLists, '/bucketlists', '/bucketlists/<int:id>')
 
-#create, update and delete item in bucket list
-api.add_resource(BucketListItem, '/bucketlists/<int:id>/items/<int:item_id>')
+# create, update and delete item in bucket list
+api.add_resource(BucketListItem, '/bucketlists/<int:id>/items',
+                 '/bucketlists/<int:id>/items/<int:item_id>')
