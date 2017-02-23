bd00508e5f52f95b2afa4c9003ee5cfb2f796b92
andela-ik <ian.kingori@andela.com>
2017-02-23 14:46:43 +0300

    [Feature] Implemented Unique Bucket List Name validation per person

--- bucketlist/decorators/validators.py
+++ bucketlist/decorators/validators.py
@@ -1,6 +1,7 @@
 from functools import wraps
-from bucketlist.models import db, User
+from bucketlist.models import db, User, BucketList
 from validate_email import validate_email
+from flask import request
 import re
 
 MIN_PASSWORD_LENGTH = 8
@@ -30,3 +31,39 @@ def validate_user_input(func):
 def validate_password(password):
     if re.search(PASSWORD_REGEX, password):
         return True
+
+
+def validate_bucket_list_data(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if "name" in request.form:
+            bucketlist_name = request.form["name"]
+        print(kwargs)
+        user_id = kwargs['user']
+        user_bucket_lists = BucketList.query.filter_by(
+            name=bucketlist_name.title(), created_by=user_id).all()
+        if len(user_bucket_lists) > 0:
+            return {"error": "error message"}, 409
+
+        return func(*args, **kwargs)
+    return decorated_function
+
+
+def validate_bucket_list_item_data(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if "name" in request.form:
+            item_name = request.form["name"]
+
+        if "id" in kwargs:
+            bucket_id = kwargs["id"]
+
+        print(kwargs)
+        user_id = kwargs['user']
+        user_bucket_lists = BucketList.query.filter_by(
+            name=bucketlist_name.title(), created_by=user_id).all()
+        if len(user_bucket_lists) > 0:
+            return {"error": "error message"}, 409
+
+        return func(*args, **kwargs)
+    return decorated_function
