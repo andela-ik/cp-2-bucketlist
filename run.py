8591cb8e1b6fa5a0bf53db4cdbd1f0982e9e2a62
andela-ik <ian.kingori@andela.com>
2017-02-23 06:45:19 +0300

    [Chore] Refactored app configurations

--- run.py
+++ run.py
@@ -2,15 +2,20 @@ from flask import Flask, request
 from flask_restful import Api
 from bucketlist.models import db
 from bucketlist.routes import api
-from bucketlist.views import Index
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config[
+    'SECRET_KEY'] = "\xffI\x9b\xb4\x147\n\x88y+2\xeef\xd1\x1d\xae\xa8\xfa\xdf\xb7"
+app.config["JSON_SORT_KEYS"] = False
+app.config["DEBUG"] = False
+
 db.app = app
 db.init_app(app)
 db.create_all()
 api.init_app(app)
-#postgresql://username:password@host:port/database
+# postgresql://username:password@host:port/database
 
 if __name__ == "__main__":
     app.run()
