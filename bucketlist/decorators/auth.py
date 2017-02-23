8a35058c695caf6664bfe1b9fce1eb4ead027442
andela-ik <ian.kingori@andela.com>
2017-02-23 06:39:02 +0300

    [Bug] Fixed decorators return values to include kwargs

--- bucketlist/decorators/auth.py
+++ bucketlist/decorators/auth.py
@@ -7,17 +7,19 @@ from flask import request
 secret = "\xffI\x9b\xb4\x147\n\x88y+2\xeef\xd1\x1d\xae\xa8\xfa\xdf\xb7"
 
 
-def check_api_key(func):
+def check_access_token(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if not "Api-Key" in request.headers:
+        if not "Access_Token" in request.headers:
             return{"error": "Unauthorized"}, 401
         try:
-            user_id = jwt.decode(request.headers['Api-Key'], secret)
+            print(request.headers)
+            user_id = jwt.decode(request.headers['Access_Token'], secret)
             user = User.query.filter_by(id=user_id['id']).first()
             if not user:
                 return{"error": "Invalid Credentials"}, 401
-            return func(*args, user=user_id['id'])
+            print(kwargs)
+            return func(*args, **kwargs, user=user_id['id'])
         except InvalidTokenError:
             return{"error": "Invalid Credentials"}, 401
     return wrapper
