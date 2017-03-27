f9326bc8167c0ccbc02588e1f2128686efe168a0
andela-ik <ian.kingori@andela.com>
2017-03-27 15:57:03 +0300

    [Feature] Added Versioning

--- bucketlist/routes.py
+++ bucketlist/routes.py
@@ -3,16 +3,18 @@ from bucketlist.views import Auth, BucketLists, BucketListItem, Index
 
 api = Api()
 
+version = "/api/v1"
 # route to github repository
-api.add_resource(Index, '/')
+api.add_resource(Index, '{}/'.format(version))
 
 # route to handle user registration and login
-api.add_resource(Auth, '/auth/<string:action>')
+api.add_resource(Auth, '{}/auth/<string:action>'.format(version))
 
 # create and list bucket lists
 # get, update and delete single bucket list
-api.add_resource(BucketLists, '/bucketlists', '/bucketlists/<int:id>')
+api.add_resource(BucketLists, '{}/bucketlists'.format(version),
+                 '{}/bucketlists/<int:id>'.format(version))
 
 # create, update and delete item in bucket list
-api.add_resource(BucketListItem, '/bucketlists/<int:id>/items',
-                 '/bucketlists/<int:id>/items/<int:item_id>')
+api.add_resource(BucketListItem, '{}/bucketlists/<int:id>/items'.format(version),
+                 '{}/bucketlists/<int:id>/items/<int:item_id>'.format(version))
