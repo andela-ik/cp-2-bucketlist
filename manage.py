dd7518d233ef385bf8a89202fa35652dfb75f672
andela-ik <ian.kingori@andela.com>
2017-03-27 14:49:05 +0300

    [Chore] Refactored

--- manage.py
+++ manage.py
@@ -1,17 +1,13 @@
-from flask import Flask, request
-from flask_restful import Api
 from bucketlist.models import db
 from bucketlist.routes import api
-from flask_script import Manager
+from flask import Flask, request
 from flask_migrate import Migrate, MigrateCommand
+from flask_restful import Api
+from flask_script import Manager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config[
-    'SECRET_KEY'] = "\xffI\x9b\xb4\x147\n\x88y+2\xeef\xd1\x1d\xae\xa8\xfa\xdf\xb7"
-app.config["JSON_SORT_KEYS"] = False
-app.config["DEBUG"] = True
+
+app.config.from_object('config.Development')
 
 db.app = app
 db.init_app(app)
@@ -21,7 +17,6 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 api.init_app(app)
-# postgresql://username:password@host:port/database
 
 if __name__ == "__main__":
     manager.run()
